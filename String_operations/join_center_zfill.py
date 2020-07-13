5706a243a674a8cc82eea916c751518713aa37fa
Suyash Chavan <31501239+suyash-20@users.noreply.github.com>
2020-07-13 18:03:35 +0530

    Create join_center_zfill.py

---
+++ String_operations/join_center_zfill.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 12 00:41:37 2020
+
+@author: Suyash Chavan
+"""
+
+x = "LOLOLOLOLOLOL"
+
+y = ' '.join(x)
+
+print(x)
+print(y)
+
+#OUTPUT
+
+#LOLOLOLOLOLOL
+#L O L O L O L O L O L O L
+
+print(x.center(20,'%'))
+
+print(x.zfill(123))
